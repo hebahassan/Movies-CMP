72154817a5cfe63ac281402bbc897d40b4f8c942
hebahassan <heba.hassan.elnashar@gmail.com>
2026-04-05 17:19:43 +0200

    Add share functionality for iOS platform

--- core/common/src/iosMain/kotlin/com/example/core/common/ShareAction.ios.kt
+++ core/common/src/iosMain/kotlin/com/example/core/common/ShareAction.ios.kt
@@ -1,8 +1,18 @@
 package com.example.core.common
 
+import platform.UIKit.UIActivityViewController
+import platform.UIKit.UIApplication
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class ShareService {
     actual fun shareMovie(movieId: Int, movieTitle: String) {
-
+        val text = "Check out $movieTitle"
+        val activityVC = UIActivityViewController(
+            activityItems = listOf(text),
+            applicationActivities = null
+        )
+        UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
+            activityVC, animated = true, completion = null
+        )
     }
 }
